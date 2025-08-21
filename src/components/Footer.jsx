04da1d44f74452a02e1c7c0dcdbb37f948b61313
Rosa Choi <122864464+momorosa@users.noreply.github.com>
2025-08-21 08:32:58 -0700

    feat(FancyLink): implement custom styled link component and upated footer tagline.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
+import { FancyLink } from "./FancyLink"
+
 export default function Footer() {
     return(
         <footer className="w-full h-16 text-center text-warm-gray-light p-4 bg-transparent">
             ©2025 - Rosa Choi.{' '}
-            <a href="/my-toolkit" secondary id="primary-link-text">
+            <FancyLink href="/my-toolkit" secondary id="primary-link-text">
                 Crafted with Endless Curiosity.
-            </a>
+            </FancyLink>
         </footer>
     )
 }
